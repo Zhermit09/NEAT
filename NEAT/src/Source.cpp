5fd4dcde9f7dc29e60853a6f8da6eb032fee1cab
Rafael Tekie <rafael.tekie@learnet.se>
2021-10-21 12:50:00 +0200

    pipe class

--- NEAT/src/Source.cpp
+++ NEAT/src/Source.cpp
@@ -7,7 +7,7 @@ int screen_h = 800;
 class Bird {
 	float _x;
 	float _y;
-	
+
 
 	float animationTime;
 	float frameTime = 1.0 / 6.0;
@@ -18,7 +18,7 @@ class Bird {
 	float fallTime = 0.0;
 	float vel = 0.0;
 	float g = 9.82;
-	
+
 
 
 public:
@@ -47,7 +47,7 @@ public:
 
 	void Gravity(float dTime) {
 		fallTime += dTime;
-		float t = fallTime; 
+		float t = fallTime;
 
 		float s = (vel * t) + (g * (t * t) / 2.0);
 
@@ -58,11 +58,11 @@ public:
 		if (s < 0.0) {
 			s -= 2.0;
 		}
-		s = s*300.0 * (dTime);
+		s = s * 300.0 * (dTime);
 
 		_y += s;
 
-		
+
 		//Temp
 		if (_y > screen_h) {
 			fallTime = 0.0;
@@ -84,15 +84,48 @@ public:
 		return _y;
 	}
 };
+class Pipe {
+	float _x;
+	float _y;
+	olc::Sprite* _pipe;
+
+public:
+	Pipe(float x, float y, olc::Sprite* pipe) {
+		_x = x;
+		_y = y;
+		_pipe = pipe;
+	}
+public:
+	float x() {
+		return _x;
+	}
+	float y() {
+		return _y;
+	}
+	olc::Sprite* pipe() {
+		return _pipe;
+	}
+	void Move(float dTime) {
+		if (_x < 0)
+			_x = screen_w;
+		_x = _x - 300.0 * (dTime);
+	}
+};
+
 
 class Engine : public olc::PixelGameEngine {
 
+	Pipe* pipe;
 	Bird* bird;
-	olc::Sprite* sprites[5] = { new olc::Sprite("./Images/bird1.png"),
-								new olc::Sprite("./Images/bird2.png"),
-								new olc::Sprite("./Images/bird3.png"),
-								new olc::Sprite("./Images/bird2.png"),
-								new olc::Sprite("./Images/bird1.png") };
+	olc::Sprite* sprites[5] =
+	{
+		new olc::Sprite("./Images/bird1.png"),
+		new olc::Sprite("./Images/bird2.png"),
+		new olc::Sprite("./Images/bird3.png"),
+		new olc::Sprite("./Images/bird2.png"),
+		new olc::Sprite("./Images/bird1.png")
+	};
+	olc::Sprite* pipeI = new olc::Sprite("./Images/pipe.png");
 
 public:
 	Engine() {
@@ -100,7 +133,7 @@ public:
 	}
 
 	bool OnUserCreate() override {
-
+		pipe = new Pipe(300.0, 700.0, pipeI);
 		bird = new Bird(100.0, 0.0, sprites);
 		return true;
 	}
@@ -109,10 +142,13 @@ public:
 
 		bird->Jump();
 		bird->Gravity(dTime);
+		pipe->Move(dTime);
 
 		Clear(olc::BLACK);
 		DrawSprite({ (int)round(bird->x()), (int)round(bird->y()) }, bird->Animate(dTime), 2, 0);
-
+		DrawSprite({ (int)round(pipe->x()),(int)round(pipe->y())}, pipe->pipe(), 2, 0);
+		//DrawSprite({ (int)round(pipe->x()),(int)round(pipe->y()) - 825}, pipe->pipe(), 2, 2);
+		//system("CLS");
 		return true;
 	}
 };
@@ -120,7 +156,7 @@ public:
 int main()
 {
 	Engine engine;
-	if (engine.Construct(screen_w, screen_h, 1, 1)){
+	if (engine.Construct(screen_w, screen_h, 1, 1)) {
 		engine.Start();
 	}
 
