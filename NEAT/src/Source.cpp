41323d877713410293f267f6f4ec501afd5beb6f
Pavel <pavel.urvantcev@gmail.com>
2021-11-16 20:16:15 +0100

    Restructuring plus AABB collisions done

--- NEAT/src/Source.cpp
+++ NEAT/src/Source.cpp
@@ -3,20 +3,24 @@
 #include <Windows.h>
 #include <Bcrypt.h>
 #pragma comment(lib, "bcrypt.lib")
-
+#define RandomY (600.0f - (float)(abs(Random() % 361)))
 
 const int screen_w = 576;
 const int screen_h = 800;
-const int pipe_w = 102;
-const int pipe_h = 640;
+
+const int scale = 2;
+int pipe_w;
+int pipe_h;
+int bird_w;
+int bird_h;
 
 bool g_bool = false;
 float t = 0.0f;
 
 
 struct _Bird {
-	float x;
-	float y;
+	float x = 0.0f;
+	float y = 0.0f;
 	std::vector <olc::Decal*> sprites;
 
 	float animationTime = 0.0f;
@@ -69,102 +73,6 @@ struct _Bird {
 };
 
 
-/*class Bird {
-	float _x;
-	float _y;
-
-	float animationTime;
-	float frameTime = 1.0f / 6.0f;
-
-	olc::Decal* sprites[5];
-	olc::Decal* current_image;
-
-	float fallTime = 0.0;
-	float vel = 0.0;
-	float g = 9.82f;
-	bool collide = false;
-
-public:
-	Bird(float x, float y, olc::Decal* sprt[5]) {
-		_x = x;
-		_y = y;
-		animationTime = 0.0;
-
-		for (int i = 0; i < 5; i++) {
-			sprites[i] = sprt[i];
-		}
-
-		current_image = sprt[0];
-	}
-
-	olc::Decal* Animate(float dTime) {
-
-		animationTime += dTime;
-		if (animationTime >= (5.0 * frameTime)) {
-			animationTime = 0.0;
-		}
-
-		current_image = sprites[(int)(animationTime / frameTime)];
-		return current_image;
-	}
-
-	void Gravity(float dTime) {
-		fallTime += dTime;
-		float t = fallTime;
-
-		float s = (vel * t) + (g * (t * t) / 2.0f);
-		if (s > 3.7) {
-			s = 3.7f;
-		}
-
-		if (s < 0.0) {
-			s *= 11.1f;
-			s -= 1.0f;
-		}
-		s = s * 300.0f * (dTime);
-
-		_y += s;
-
-
-		//Temp
-		if (_y > screen_h) {
-			fallTime = 0.0;
-			_y = 0.0;
-			vel = 0.0;
-		}
-	}
-	void Jump(float dTime) {
-		//if (_y > screen_h / 2.0 && _y < screen_h / 2.0 + 50.0) {
-		fallTime = 0.0;
-		vel = -0.85f;
-		//}
-	}
-
-	float x() {
-		return _x;
-	}
-	void sx(float x) {
-		_x = x;
-	}
-	float y() {
-		return _y;
-	}
-	void sy(float y) {
-		_y = y;
-	}
-	void svel(float v) {
-		vel = v;
-		fallTime = v;
-	}
-	bool hit() {
-		return collide;
-	}
-	void shit(bool boo) {
-		collide = boo;
-	}
-};*/
-
-
 
 struct Pipe {
 	float x;
@@ -179,7 +87,7 @@ struct Pipe {
 
 
 
-struct Moving {
+struct Scenery {
 	float x;
 	float speed;
 
@@ -198,18 +106,21 @@ class Engine : public olc::PixelGameEngine {
 	std::vector<olc::Decal*> sprites;
 
 	_Bird bird;
-	Moving background;
-	Moving ground;
+	olc::Pixel tint;
+	Scenery background;
+	Scenery ground;
 
 	olc::Decal* bg_image;
 	olc::Decal* ground_image;
 	olc::Decal* pipeImg_Down;
 	olc::Decal* pipeImg_Up;
 
+
 public:
 	Engine() {
 		sAppName = "Flappy Bird NEAT";
 	}
+
 
 	bool OnUserCreate() override {
 
@@ -218,30 +129,33 @@ public:
 		pipeImg_Down = new olc::Decal(new olc::Sprite("./Images/pipe.png"));
 		pipeImg_Up = new olc::Decal(new olc::Sprite("./Images/pipe1.png"));
 
-		sprites = { 
+		sprites = {
 			new olc::Decal(new olc::Sprite("./Images/bird1.png")),
 			new olc::Decal(new olc::Sprite("./Images/bird2.png")),
 			new olc::Decal(new olc::Sprite("./Images/bird3.png")),
 			new olc::Decal(new olc::Sprite("./Images/bird2.png")),
-			new olc::Decal(new olc::Sprite("./Images/bird1.png")) 
+			new olc::Decal(new olc::Sprite("./Images/bird1.png"))
 		};
 
-		//bird = new Bird(50.0f, 80.0f, sprites);
+		tint = *new olc::Pixel(255, 255, 255);
 		bird = { 50.0f, 80.0f, sprites };
 		background = { 0.0f, 20.0f };
 		ground = { 0.0f, 220.0f };
 
-		pipeList.push_back(Pipe{ (float)screen_w, 600.0f - (float)(abs(Random() % 361)) });
+		pipeList.push_back(Pipe{ (float)screen_w, RandomY });
 
-		//pipeList.push_back(new Pipe(50.0f, 600.0f - 360.0f));
-		//pipeList.push_back(new Pipe(440.0f, 600.0f - 0.0f));
+		pipe_w = scale * pipeImg_Down->sprite->width;
+		pipe_h = scale * pipeImg_Down->sprite->height;
+		bird_w = scale * sprites[0]->sprite->width;
+		bird_h = scale * sprites[0]->sprite->height;
 
 		return true;
 	}
 
+
 	bool OnUserUpdate(float dTime) override {
 
-		Action(dTime);
+		Actions(dTime);
 		Draw(dTime);
 
 		return true;
@@ -249,23 +163,24 @@ public:
 
 	//###################################################################################################################################
 
-	void Action(float dTime) {
-		//bird->Jump();
+	void Actions(float dTime) {
+
 		if (g_bool) {
+
 			bird.Gravity(dTime);
+
 			for (Pipe& pipe : pipeList) {
 				pipe.Move(dTime);
 			}
-			//pipeList[0].Move(dTime);
-
 		}
-
-		//back->Move(dTime); 
+		
 		ground.Move(dTime);
 		background.Move(dTime);
 
+		//Tools
 		DevMove(dTime);
 		t += dTime;
+		//
 
 		Collide();
 
@@ -273,11 +188,14 @@ public:
 		PipeOffScreen();
 	}
 
-	void NewPossiblePipe() {
-		int x = pipeList.front().x;
 
-		if (x < (screen_w / 2) && (pipeList.front().newPipe == true)) {
-			pipeList.push_back(Pipe{ (x + (float)pipe_w) + ((float)screen_w / 2.0f), 600.0f - (float)(abs(Random() % 361)) });
+	void NewPossiblePipe() {
+		Pipe pipe = pipeList.front();
+
+		if (pipe.x < (screen_w / 2) && (pipe.newPipe == true)) {
+			pipeList.push_back(Pipe{ (pipe.x + pipe_w) + (screen_w / 2), RandomY });
+
+			//Must get new front pipe after push_back()
 			pipeList.front().newPipe = false;
 		}
 	}
@@ -288,7 +206,7 @@ public:
 		}
 	}
 
-	void DevMove(float dTime) {
+	void DevMove(float dTime) {             //some tools
 		float speed = 100.0f * dTime;
 
 
@@ -309,25 +227,16 @@ public:
 		}
 		else if (GetAsyncKeyState('R') & 0x0101) {
 			bird.x = 50.0f + pipe_w;
-			bird.y = 600.0f - 48.0f;
+			bird.y = pipeList.front().y - bird_h;
 			bird.vel = 0.0f;
 
-			//pipeList.clear();
-			//pipeList.push_back(new Pipe(50, 600.0f - 0.0f));
-			//pipeList.push_back(new Pipe(440, 600.0f - 360.0f));
 		}
 		else if (GetAsyncKeyState('G') & 0x0101) {
-			if (g_bool) { //&& t > 0.3) {
+			if (g_bool) { 
 				g_bool = false;
-				//		t = 0.0f;
 			}
-			else if (!g_bool) {// && t > 0.1) {
+			else if (!g_bool) {
 				g_bool = true;
-				//		t = 0.0f;
-				//	}
-			}
-			if (bird.y <= 601 && bird.y >= 600) {
-				//std::cout << pipeList.back()->x() << std::endl;
 			}
 		}
 	}
@@ -340,16 +249,19 @@ public:
 			float py = pipe.y;
 			float px = pipe.x;
 
-			//std::cout << (((px <= bx) and (bx <= (px + pipe_w))) and ((py <= by) or ((py - 160) >= by))) << std::endl;
-
-			if ((px <= bx && bx <= (px + pipe_w)) && (!(py <= by) && !(by <= (py + pipe_h)))) {
+			if (((bx < px + pipe_w) and (px < (bx + bird_w))) and (((by + bird_h) > py) or (py - (screen_h - pipe_h) > by))) {
 				bird.collide = true;
-				std::cout << "Collide" << std::endl;
+				tint.g = 0;
+				tint.b = 0;
+				break;
 			}
-			else { bird.collide = false; }
-			//std::cout << bird->hit() << std::endl;
+			//Temp???
+			else {
+				bird.collide = false;
+				tint.g = 255;
+				tint.b = 255;
+			}
 		}
-		//	std::cout << std::endl;
 	}
 
 	//###################################################################################################################################
@@ -359,30 +271,21 @@ public:
 		float bg_x = round(background.x);
 		float g_x = round(ground.x);
 
-		DrawDecal({ bg_x, -125 }, bg_image, { 2,2 });
-		DrawDecal({ (bg_x + screen_w), -125 }, bg_image, { 2,2 });
+		DrawDecal({ bg_x, -125 }, bg_image, { scale,scale });
+		DrawDecal({ (bg_x + screen_w), -125 }, bg_image, { scale,scale });
 
 		for (Pipe& pipe : pipeList) {
 			float y = round(pipe.y);
 			float x = round(pipe.x);
 
-			//std::cout << pipe.x << std::endl;
-
-			DrawDecal({ x, y }, pipeImg_Down, { 2,2 });
-			DrawDecal({ x, (y - 800) }, pipeImg_Up, { 2,2 });
+			DrawDecal({ x, y }, pipeImg_Down, { scale,scale });
+			DrawDecal({ x, (y - screen_h) }, pipeImg_Up, { scale,scale });
 		}
 
-		if (bird.collide) {
-			//SetPixelMode(olc::Pixel::MASK);
-			DrawDecal({ round(bird.x), round(bird.y) }, bird.Animate(dTime), { 2,2 }, *new olc::Pixel(255, 0, 0, 180));
-			//SetPixelMode(olc::Pixel::NORMAL);
-		}
-		else {
-			DrawDecal({ round(bird.x), round(bird.y) }, bird.Animate(dTime), { 2,2 });
-		}
+		DrawDecal({ round(bird.x), round(bird.y) }, bird.Animate(dTime), { scale,scale }, tint);
 
-		DrawDecal({ g_x, 675 }, ground_image, { 2,2 });
-		DrawDecal({ g_x + screen_w, 675 }, ground_image, { 2,2 });
+		DrawDecal({ g_x, 675 }, ground_image, { scale,scale });
+		DrawDecal({ g_x + screen_w, 675 }, ground_image, { scale,scale });
 	}
 
 
@@ -401,8 +304,10 @@ public:
 
 int main()
 {
+	int pixelSize = 1;
+
 	Engine engine;
-	if (engine.Construct(screen_w, screen_h, 1, 1)) {
+	if (engine.Construct(screen_w, screen_h, pixelSize, pixelSize)) {
 		engine.Start();
 	}
 
